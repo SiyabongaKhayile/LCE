a35f6a0ca369246d1990504c889c20dccf3e5249
SiyabongaKhayile <siyabongabongumusa895@gmail.com>
2025-03-20 08:31:19 +0200

    yaya

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,22 +11,7 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  },
-  {
-    path: 'signup',  // Changed from 'sign-up' to 'signup'
-    loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpPageModule)
-  },
-  {
-    path: 'news',
-    loadChildren: () => import('./news/news.module').then( m => m.NewsPageModule)
-  },
-  {
-    path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule)
-  },
+ 
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  currentSection: string = 'home';
+  backgroundImages: string[] = [
+    '/assets/images/Inkosinathi.jpg',
+    '/assets/images/Yaya.jpg'
+    // Add more image paths here as needed
+  ];
+  currentBackgroundIndex: number = 0;
+  
   constructor() {}
-
+  
+  ngOnInit() {
+    // Start the background image rotation
+    this.startBackgroundRotation();
+  }
+  
+  showSection(section: string) {
+    this.currentSection = section;
+  }
+  
+  startBackgroundRotation() {
+    // Change background image every 10 seconds
+    setInterval(() => {
+      this.currentBackgroundIndex = (this.currentBackgroundIndex + 1) % this.backgroundImages.length;
+      this.updateBackgroundImage();
+    }, 10000);
+  }
+  
+  updateBackgroundImage() {
+    const content = document.querySelector('ion-content') as HTMLElement;
+    if (content) {
+      content.style.backgroundImage = `url('${this.backgroundImages[this.currentBackgroundIndex]}')`;
+    }
+  }
 }
